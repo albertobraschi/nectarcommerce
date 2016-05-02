c7a7c87631cb614a81255a1a05832e5f1bcc9075
Nimish <nimish@vinsol.com>
2016-05-02 15:08:18 +0530

    update travis.yml

--- apps/nectar/web/static/js/views/base_view.js
+++ apps/nectar/web/static/js/views/base_view.js
@@ -1,5 +1,13 @@
+import ReactDOM       from "react-dom";
+import { Provider }   from "react-redux";
+
+import { MiniCart }   from "../components";
+import configureStore from "../store";
+import cartActions    from "../actions/cart";
+
 export default class BaseView {
   mount() {
+    this.store = configureStore();
     console.log("common action for application");
   }
 
