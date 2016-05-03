b6f8ef905a693945de14f05d9e94f2b7786f0fa1
Nimish <nimish@vinsol.com>
2016-05-03 15:36:05 +0530

    sync across multiple browsers

--- apps/nectar/web/channels/cart_daemon.ex
+++ apps/nectar/web/channels/cart_daemon.ex
@@ -1,7 +1,7 @@
 defmodule CartDaemon do
   use GenServer
 
-  defmodule State, do: defstruct cart_id: nil, counter: 0
+  defmodule State, do: defstruct cart_id: nil, counter: 0, running: false
 
   def get(cart_id) do
     daemon_name = String.to_atom "cart_daemon_#{cart_id}"
@@ -16,8 +16,19 @@ defmodule CartDaemon do
     GenServer.start(__MODULE__, state, name: name)
   end
 
-  def handle_cast({:monitor}, state) do
+  def handle_cast({:monitor}, %State{running: false} = state) do
     # process to check if out of stock
+    Process.send_after(self(), {:check_out_of_stock}, 50)
+    # call self every 20000ms
+    Process.send_after(self(), {:monitor}, 4000)
+    {:noreply, %State{state|counter: state.counter + 1, running: true}}
+  end
+
+  def handle_cast({:monitor}, %State{} = state) do
+    {:noreply, state}
+  end
+
+  def handle_cast({:re_monitor}, state) do
     Process.send_after(self(), {:check_out_of_stock}, 50)
     # call self every 20000ms
     Process.send_after(self(), {:monitor}, 4000)
@@ -25,12 +36,13 @@ defmodule CartDaemon do
   end
 
   def handle_info({:check_out_of_stock}, state) do
+    IO.inspect "#{inspect(self())} sending to cart#{state.cart_id}, msg #state.counter"
     Nectar.Endpoint.broadcast "cart:#{state.cart_id}", "new_notification", %{msg: "out of stock notification #{state.counter}"}
     {:noreply, state}
   end
 
   def handle_info({:monitor}, state) do
-    GenServer.cast(self(), {:monitor})
+    GenServer.cast(self(), {:re_monitor})
     {:noreply, state}
   end
 
