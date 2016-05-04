b6bca9bf6e0bb22b26083f4e925f4c64c9098696
Nimish <nimish@vinsol.com>
2016-05-04 14:58:52 +0530

    fix the js loading for country show page

--- apps/nectar/web/static/js/views/admin/countries/show_view.js
+++ apps/nectar/web/static/js/views/admin/countries/show_view.js
@@ -1,8 +1,11 @@
 import BaseCountryView from "./base_country_view";
+import State from "./state";
 
 export default class ShowView extends BaseCountryView {
   mount() {
     super.mount();
+    const countryId = document.getElementById("country").getAttribute("data-country-id");
+    State.init(parseInt(countryId, 10));
   }
 
   unmount() {
--- apps/nectar/web/static/js/views/admin_base_view.js
+++ apps/nectar/web/static/js/views/admin_base_view.js
@@ -1,6 +1,8 @@
+import AjaxSetup from "../lib/ajax_setup";
+
 export default class AdminBaseView {
   mount() {
-
+    AjaxSetup.setup();
   }
   unmount() {
 
