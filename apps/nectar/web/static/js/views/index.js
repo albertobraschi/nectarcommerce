8a30407596b0933c6290cf4324dedf1194409fa1
Nimish <nimish@vinsol.com>
2016-05-10 18:27:53 +0530

    add the product edit view

--- apps/nectar/web/static/js/views/index.js
+++ apps/nectar/web/static/js/views/index.js
@@ -1,15 +1,23 @@
-import BaseView      from "./base_view";
-import AdminBaseView from "./admin_base_view";
-import ProductView   from "./products";
-import AdminCountryView from "./admin/countries";
-import AdminOrderView from "./admin/orders";
-import AdminCartView from "./admin/cart";
-import AdminZoneView from "./admin/zones";
+import BaseView          from "./base_view";
+import AdminBaseView     from "./admin_base_view";
+import ProductView       from "./products";
+import AdminCountryView  from "./admin/countries";
+import AdminOrderView    from "./admin/orders";
+import AdminCartView     from "./admin/cart";
+import AdminZoneView     from "./admin/zones";
 import AdminCheckoutView from "./admin/checkout";
-import CheckoutView from "./checkout
+import CheckoutView      from "./checkout";
+import AdminProductView  from "./admin/products";
 
 // add all the views here.
-const views = {ProductView, AdminCountryView, AdminOrderView, AdminCartView, AdminZoneView, AdminCheckoutView, CheckoutView};
+const views = {ProductView,
+               AdminCountryView,
+               AdminOrderView,
+               AdminCartView,
+               AdminZoneView,
+               AdminCheckoutView,
+               CheckoutView,
+               AdminProductView};
 
 export default function viewToRender(view) {
   let viewLookUp   = view.split(".");
